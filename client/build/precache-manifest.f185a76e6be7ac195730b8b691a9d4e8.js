ab5dec5f53735c1b8d01e34d8dbf6fdbaa3dcd8f
Alex Herce <alejandro@herce.co>
2019-05-03 15:22:16 -0500

    build ready

---
+++ client/build/precache-manifest.f185a76e6be7ac195730b8b691a9d4e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a145cb60ad1c61e64c77",
+    "url": "/static/js/main.b757a4de.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "a145cb60ad1c61e64c77",
+    "url": "/static/css/main.cf96f79f.chunk.css"
+  },
+  {
+    "revision": "37be1666d3278c2b09dd60666487131d",
+    "url": "/index.html"
+  }
+];
